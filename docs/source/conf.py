db645462c354997099cf9d5a11b0f00750dde38a
Felipe Odorizi de Mello <felipeodorizi@gmail.com>
2022-03-09 13:06:23 -0300

    Add files via upload

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Felipe'
+copyright = '2021, Felipe'
+author = 'Felipe'
 
 release = '0.1'
 version = '0.1.0'
--- lumache.py
+++ lumache.py
@@ -1,5 +1,5 @@
 """
-Lumache - Python library for cooks and food lovers.
+PCLIMA - Python library lumache.py
 """
 
 __version__ = "0.1.0"
